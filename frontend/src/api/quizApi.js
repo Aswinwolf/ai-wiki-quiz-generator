541f8e856daa0cd7b0a6f10e621bee85c0240f87
Aswin Siva <aswinwolf1234@gmail.com>
2026-01-11 12:44:22 +0000

    Fix env config for Vite and prepare for deployment

--- frontend/src/api/quizApi.js
+++ frontend/src/api/quizApi.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+console.log("API BASE URL =", BASE_URL);
 
 export const generateQuiz = async (url, numQuestions) => {
   const res = await axios.post(`${BASE_URL}/generate-quiz`, {
